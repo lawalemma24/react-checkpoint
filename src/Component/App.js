ce8f4ae35cab27795514d78f52662072b92ab2c0
lawalemma24 <lawalemma24@gmail.com>
2023-08-25 17:18:19 +0100

    react-checkpoint

---
+++ src/Component/App.js
@@ -0,0 +1,24 @@
+// import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import React, { Component } from 'react'
+import react from 'react';
+import Header from './Header'
+// import Food from './Component/Food/Food';
+import Navbar from './Navbar'
+
+
+
+function App () {
+return (
+  <>
+  {/* <Food/> */}
+  <Navbar/>
+  <Header/>
+  {/* <Navbar/> */}
+  </>
+)
+
+
+}
+
+export default App
